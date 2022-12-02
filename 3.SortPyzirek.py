076b017a695bcf61a9f2c4a385e78190bc8674d3
Anna Izyumskaya <annaiziumskaya@yandex.ru>
2022-12-02 10:05:48 +0500

    Сортировка по возрастанию, убыванию

--- 3.SortPyzirek.py
+++ 3.SortPyzirek.py
@@ -1,14 +1,25 @@
 from random import randint
+D=int(input("Сортировка по возрастанию - 1\n"
+        "Сортировка по убыванию - 2\n"))
 
-N = int(input("Ввдите количство чисел"))
+N = int(input("Ввдите количство чисел\n"))
 a = []
 for i in range(N):
     a.append(randint(1, N))
 print(a)
 
-for i in range(N - 1):
-    for j in range(N - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
+if D==1:
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
 
-print(a)
+    print(a)
+
+else:
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] < a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+    print(a)
